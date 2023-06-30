8807afd9e8e901d638ea8c4fbc6f0a5645d55a93
Prosleo <95958259+Prosleo@users.noreply.github.com>
2023-06-30 09:59:46 +0300

    Add randomized words to main.py
    updated to now use random

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 import random
-import time
 
-
-eng_words = ['Hi','Bye','Task', 'Programm']
-ru_words = ['Привет','Пока','Задача', 'Программа']
+eng_words = ['hi','bye','Task', 'program']
+ru_words = ['привет','пока','задача', 'программа']
 score = 0
 
 mod = input("Выбери режим работы тренажера: 0 - добавить новые слова, 1 - тренироваться: \n")
@@ -13,16 +11,17 @@ while ((mod != '0') and (mod != '1')):
 if mod == "1":
     print("Переведи как можно больше слов правильно! У тебя 10 попыток!")
     for i in range(10):
-        number = random.randint(0, len(eng_words))
-        print("Как переводится слово: " + eng_words[number])
-        if input() == ru_words[number]:
+        number = random.randint(0, len(eng_words) - 1)
+        eng_word = eng_words[number]
+        print("Как переводится слово: " + eng_word)
+        if input().lower() == ru_words[number].lower():
             print("Отлично!!!")
             score += 1
         else:
             print("Нет... Это слово - " + ru_words[number])
 else:
-    word = input("Введите слово на русском языке: ")
-    translate = input("Введите перевод этого слова: ")
+    word = input("Введите слово на русском языке: ").lower()
+    translate = input("Введите перевод этого слова: ").lower()
     if len(word) > 0 and len(translate) > 0:
         ru_words.append(word)
         eng_words.append(translate)
